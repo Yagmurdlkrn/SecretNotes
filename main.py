673bf13f494d77421887d94b5581db3f726d0a0c
Yağmur Dalkıran <yagmurdalkiran1997@gmail.com>
2023-12-29 14:26:27 +0300

    image added

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 window = Tk()
 window.title("Secret Quiz")
-window.minsize(width=300,height=600)
+window.minsize(width=300, height=600)
 
+# Resmi aç
+image = Image.open("topsecret.png")
+
+# Resmi istediğiniz boyutlara boyutlandır
+resized_image = image.resize((200, 120))
+
+# Tkinter PhotoImage'a dönüştür
+image = ImageTk.PhotoImage(resized_image)
+
+imageLabel = Label(window, image=image)
+imageLabel.pack()
 
 window.mainloop()
