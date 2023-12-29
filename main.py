eb9340e80c4242dcbb35f89d3fe2281457ce0f83
Yağmur Dalkıran <yagmurdalkiran1997@gmail.com>
2023-12-29 14:40:11 +0300

    label,textand button added

--- main.py
+++ main.py
@@ -5,16 +5,36 @@ window = Tk()
 window.title("Secret Quiz")
 window.minsize(width=300, height=600)
 
-# Resmi aç
+
 image = Image.open("topsecret.png")
 
-# Resmi istediğiniz boyutlara boyutlandır
+
 resized_image = image.resize((200, 120))
 
-# Tkinter PhotoImage'a dönüştür
+
 image = ImageTk.PhotoImage(resized_image)
 
 imageLabel = Label(window, image=image)
 imageLabel.pack()
+
+label1 = Label(text="Enter your title")
+label1.pack()
+text1 = Text(width=30,height=2)
+text1.pack()
+
+label2 = Label(text="Enter your secret")
+label2.pack()
+text2 = Text(width=30,height=15)
+text2.pack()
+
+label3 = Label(text="Enter your master key")
+label3.pack()
+text3 = Text(width=30,height=2)
+text3.pack()
+
+button1 = Button(text="Save & Encrypt")
+button2 = Button(text="Decrypt")
+button1.pack()
+button2.pack()
 
 window.mainloop()
